ccd62931d5e9ad416a32ceb3ffe3d3135ea5e84f
Geri <gregor.horvath83@gmail.com>
2020-11-17 20:19:55 +0200

    module-08-delegation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('JS Group 24');
+const menu = document.querySelector('.js-nav');
+
+const handleMenuClick = e => {
+  const current = e.target;
+  const prev = document.querySelector('.active');
+
+  if (e.currentTarget === e.target) {
+    return;
+  }
+
+  // if (e.target.nodeName !== 'A') {
+  //   return;
+  // }
+
+  if (prev) {
+    prev.classList.remove('active');
+  }
+
+  current.classList.add('active');
+};
+
+menu.addEventListener('click', handleMenuClick);
