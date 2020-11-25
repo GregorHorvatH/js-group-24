915666e1b3cde29b6eb9e6430cb26972963e9bb1
Geri <gregor.horvath83@gmail.com>
2020-11-25 21:01:56 +0200

    module-09-webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('JS Group 24');
+import { uuid } from 'uuidv4';
+import './styles.css';
+
+import dog from './images/dog.jpeg';
+
+console.log(uuid());
+
+// ======== functions ========
+const sayHello = () => {
+  console.log('Hello');
+};
+
+const sum = (a, b) => {
+  console.log(a + b);
+};
+
+// ======== run ========
+sayHello();
+sum(20, 9);
+
+const img = document.querySelector('img');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  img.src = dog;
+});
